7eaaa089abc31d33eba723845e03cfdf91c205fd
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-08-03 21:38:23 -0700

    add new npm

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const myModule = require('./favFoodsModule.js');
-//console.log("hello world")
-
-// myModule.food();
-
+const myModule = require("./favFoodsModule.js");
 myModule.foodie.forEach(element => console.log(element));
+
+
+//Using Better-Array npm package
+let BetterArray = require('better-array');
+favFoods = BetterArray(["pizza", "sushi", "pasta", "tacos", "pho", "ramen"])
+console.log(favFoods.first()); //returns first word in array
+console.log(favFoods.count());// counts indexes in array
+//insert new word into array
+favFoods.$insert(2, "burritos");
+console.log(favFoods);
+
+
