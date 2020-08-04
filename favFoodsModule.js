d72d0801b238b1b5bd74d15b90c1e5089782f66f
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-08-03 21:14:36 -0700

    create node array and print to screen in index.js

---
+++ favFoodsModule.js
@@ -0,0 +1,8 @@
+// module.exports.food = () => {
+//     let favFoods = ["pizza", "sushi", "pasta", "tacos", "pho", "ramen"];
+//     for (let i=0; i<favFoods.length; i++){
+//         console.log(favFoods[i]);
+//     }
+// }
+
+module.exports.foodie = ["pizza", "sushi", "pasta", "tacos", "pho", "ramen"];
---
+++ index.js
@@ -0,0 +1,6 @@
+const myModule = require('./favFoodsModule.js');
+//console.log("hello world")
+
+// myModule.food();
+
+myModule.foodie.forEach(element => console.log(element));
